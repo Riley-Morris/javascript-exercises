94736193fb205d6296eefbefb62641f70cf91bae
Riley Morris <rileymichaelmorris@gmail.com>
2022-11-06 16:10:43 -0500

    exercise 5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let mySum = 0
+    if (numOne < 0 || numTwo < 0 || Number(numOne) !== numOne|| Number(numTwo) !== numTwo||Array.isArray(numOne)||Array.isArray(numTwo)){
+        mySum = 'ERROR'
+    }else if (numOne < numTwo) {
+        for (i = numOne; i<= numTwo; i++){
+            mySum += i
+        }
+    }else if (numTwo < numOne) {
+        for (i = numTwo; i<= numOne; i++){
+            mySum += i
+        }
+    }
+    return mySum
 };
 
 // Do not edit below this line
