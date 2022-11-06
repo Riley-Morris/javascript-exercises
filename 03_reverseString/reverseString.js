a2c403874094e27a4c9afe285d8a39e82eb150ee
Riley Morris <rileymichaelmorris@gmail.com>
2022-11-06 12:51:08 -0500

    exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(givenString) {
+    let newString = ''
+    let givenArray = Array.from(givenString)
+    for (char of givenString){
+        const popped = givenArray.pop()
+        newString += popped
+    }
+    return newString
 };
 
 // Do not edit below this line
