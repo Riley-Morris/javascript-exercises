ef6645284c2eeb1cd40abd3dcf5546325711811d
Riley Morris <rileymichaelmorris@gmail.com>
2022-11-06 12:28:57 -0500

    exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(givenString, num) {
+    repeated = ''
+    if (num < 0){
+        repeated = 'ERROR'
+    }else {for (i = 1; i <= num; i++) {
+        repeated += givenString
+    }
+}
+    return repeated
 };
 
 // Do not edit below this line
