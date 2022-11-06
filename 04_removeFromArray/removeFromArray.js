8b237700c85b40ed5e59a9fd80c0757cd5833e5d
Riley Morris <rileymichaelmorris@gmail.com>
2022-11-06 15:43:09 -0500

    exercise 4 (removeFromArray)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(someArray, ...removeItem) {
+    for (n=0; n<removeItem.length; n++){    
+        for (i=0; i<someArray.length; i++) {
+            if (someArray[i]===removeItem[n]) {
+                someArray.splice(i, 1)
+                }
+            }
+        }
+    return someArray
 };
 
 // Do not edit below this line
