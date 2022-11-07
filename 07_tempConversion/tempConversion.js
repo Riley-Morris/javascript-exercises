989e0096dd2eea34426d1bf6507afef0bda54883
Riley Morris <rileymichaelmorris@gmail.com>
2022-11-07 15:30:47 -0500

    exercise 7 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(degree) {
+  let converted = (degree - 32) * (5/9)
+  let rounded = converted.toFixed(1)
+  return +rounded
 };
 
-const ctof = function() {
-
+const ctof = function(degree) {
+  let converted = (degree * (9/5)) + 32
+  let rounded = converted.toFixed(1)
+  return +rounded
 };
 
 // Do not edit below this line
